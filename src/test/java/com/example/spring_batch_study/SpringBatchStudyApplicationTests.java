4c2f7d4268fddfb18cc124c34e4ed7e85ffbacd3
Joong Seok Hwang <hjs7949@naver.com>
2024-08-10 13:09:12 +0900

    🎉init : Init project

---
+++ src/test/java/com/example/spring_batch_study/SpringBatchStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_batch_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
